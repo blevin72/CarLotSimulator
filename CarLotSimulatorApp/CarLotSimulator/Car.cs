242dd0fe99929b80729ad7f5a91bb70db746022c
Bryan Levin <bryanlevin72@gmail.com>
2023-06-30 08:27:59 -0700

    Exercise 2

---
+++ CarLotSimulatorApp/CarLotSimulator/Car.cs
@@ -0,0 +1,40 @@
+﻿using System;
+namespace CarLotSimulator
+{
+	public class Car
+	{
+		public Car()
+		{
+		}
+
+	public Car(int year, string make, string model, string engineNoise, string honkNoise, string isDriveable)
+		{
+            Year = year;
+            Make = make;
+            Model = model;
+            EngineNoise = engineNoise;
+            HonkNoise = honkNoise;
+			IsDriveable = isDriveable;
+        }
+        //Car shall have the following properties: Year, Make, Model, EngineNoise, HonkNoise, IsDriveable
+        //Car shall have the following methods: MakeEngineNoise(), MakeHonkNoise()
+        //The methods should take one string parameter: the respective noise property
+
+        public int Year { get; set; }
+		public string Make { get; set; }
+		public string Model { get; set; }
+		public string EngineNoise { get; set; }
+		public string HonkNoise { get; set; }
+		public string IsDriveable { get; set; }
+
+		public void MakeEngineNoise(string EngineNoise)
+		{
+			Console.WriteLine($"The {Year} {Make} {Model} engine makes a {EngineNoise} noise.");
+		}
+
+		public void MakeHonkNoise(string HonkNoise)
+		{
+			Console.WriteLine($"The {Year} { Make} {Model} horn makes a {HonkNoise} noise.");
+		}
+	}
+}
--- CarLotSimulatorApp/CarLotSimulator/Program.cs
+++ CarLotSimulatorApp/CarLotSimulator/Program.cs
@@ -8,26 +8,74 @@ namespace CarLotSimulator
         {
             //TODO
 
-            //Create a seperate class file called Car
-            //Car shall have the following properties: Year, Make, Model, EngineNoise, HonkNoise, IsDriveable
-            //Car shall have the following methods: MakeEngineNoise(), MakeHonkNoise()
-            //The methods should take one string parameter: the respective noise property
-
-
             //Now that the Car class is created we can instanciate 3 new cars
+            Car car1 = new Car();
+            Car car2 = new Car();
+            Car car3 = new Car();
+
             //Set the properties for each of the cars
+            car1.Year = 1993;
+            car1.Make = "Honda";
+            car1.Model = "Civic";
+            car1.EngineNoise = "Soft";
+            car1.HonkNoise = "Noraml";
+            car1.IsDriveable = "Yes";
+
+            car2.Year = 2005;
+            car2.Make = "Toyota";
+            car2.Model = "Tundra";
+            car2.EngineNoise = "Loud";
+            car2.HonkNoise = "Loud";
+            car2.IsDriveable = "Yes";
+
+            car3.Year = 1995;
+            car3.Make = "Buick";
+            car3.Model = "Lesabre";
+            car3.EngineNoise = "Average";
+            car3.HonkNoise = "Squeaky";
+            car3.IsDriveable = "No";
+
             //Call each of the methods for each car
+            car1.MakeEngineNoise(car1.EngineNoise);
+            car1.MakeHonkNoise(car1.HonkNoise);
+
+            car2.MakeEngineNoise(car2.EngineNoise);
+            car2.MakeHonkNoise(car2.HonkNoise);
+
+            car3.MakeEngineNoise(car3.EngineNoise);
+            car3.MakeHonkNoise(car3.HonkNoise);
 
             //*************BONUS*************//
 
-            // Set the properties utilizing the 3 different ways we learned about, one way for each car
+                // Set the properties utilizing the 3 different ways we learned about, one way for each car
+                //dot notation
+                car1.Year = 1993;
+                car1.Make = "Honda";
+                car1.Model = "Civic";
+                car1.EngineNoise = "Soft";
+                car1.HonkNoise = "Normal";
+                car1.IsDriveable = "Yes";
 
-            //*************BONUS X 2*************//
+                //Object Initializer Syntax
+                Car car4 = new Car()
+                {
+                 Year = 2005,
+                 Make = "Toyota",
+                 Model = "Tundra",
+                 EngineNoise = "Loud",
+                 HonkNoise = "Loud",
+                 IsDriveable = "Yes",
 
-            //Create a CarLot class
-            //It should have at least one property: a List of cars
-            //Instanciate the a Carlot at the beginning of the program and as you create a car add the car to the list.
-            //At the end iterate through the list printing each of car's Year, Make, and Model to the console
+                };
+
+            //Custom Constructor
+            Car car5 = new Car(2019, "Ford", "Explorer", "Loud", "Normal", "No");
+                //*************BONUS X 2*************//
+
+                //Create a CarLot class
+                //It should have at least one property: a List of cars
+                //Instanciate the a Carlot at the beginning of the program and as you create a car add the car to the list.
+                //At the end iterate through the list printing each of car's Year, Make, and Model to the console
         }
     }
 }
